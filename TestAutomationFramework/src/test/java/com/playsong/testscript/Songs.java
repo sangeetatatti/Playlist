982d6054ae23f5392145dd9517801bdf2ff9f121
sangeetatatti <117430265+sangeetatatti@users.noreply.github.com>
2023-07-12 18:21:15 +0530

    Creating new project

---
+++ TestAutomationFramework/src/test/java/com/playsong/testscript/Songs.java
@@ -0,0 +1,28 @@
+package com.playsong.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Songs {
+	@Test
+	public void S1() {
+		Reporter.log("S1",true);
+	}
+
+	@Test
+	public void S2() {
+		Reporter.log("S2",true);
+	}
+	
+	@Test
+	public void S3() {
+		Reporter.log("S3",true);
+	}
+	
+	@Test
+	public void songS4() {
+		Reporter.log("S4",true);
+	}
+	
+	
+}
